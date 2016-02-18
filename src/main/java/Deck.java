a4b23fdc266e61d731bb84d090cb23a4fb8e8e9d
David Remington <david.ian.remington@gmail.com>
2016-02-18 15:45:50 -0800

    add initial structure

---
+++ src/main/java/Deck.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Deck {
+  private static final String[] SUITS = {"Spades", "Clubs", "Hearts", "Diamonds"};
+  private static final String[] VALUES = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+
+  private ArrayList<Card> mCards;
+
+  public Deck() {
+    mCards = ArrayList<Card>();
+    for (String suit : SUITS) {
+      for (String value : VALUES) {
+        Card newCard = new Card(suit,value);
+        mCardsList.add(newCard);
+      }
+    }
+  }
+}
